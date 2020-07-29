3549b97c5e16e6a536a63453b769400c4c950ece
Ehaab Basil <ehaabbasil1@gmail.com>
2020-07-29 08:21:57 +0800

    refactoring.

--- spike.py
+++ spike.py
@@ -1,5 +1,4 @@
-from datetime import timedelta
-import pgpy
+from pgpy import PGPKey, PGPMessage, PGPUID
 from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
 
 
@@ -10,8 +9,8 @@ USER = {
     'email': 'sriram@sriramg.com'
 }
 
-key = pgpy.PGPKey.new(ALGORITHM, 4096)
-uid = pgpy.PGPUID.new(USER['name'], comment=USER['comment'], email=USER['email'])
+key = PGPKey.new(ALGORITHM, 4096)
+uid = PGPUID.new(USER['name'], comment=USER['comment'], email=USER['email'])
 key.add_uid(uid, usage={KeyFlags.EncryptCommunications},
             hashes=[HashAlgorithm.SHA512],
             ciphers=[SymmetricKeyAlgorithm.AES256],
@@ -21,13 +20,13 @@ pub_key_str = str(key.pubkey)
 
 
 def encrypt(data: str, pub_key: str) -> str:
-    msg = pgpy.PGPMessage.new(data)
-    pub_key_obj = pgpy.PGPKey.from_blob(pub_key)[0]
+    msg = PGPMessage.new(data)
+    pub_key_obj = PGPKey.from_blob(pub_key)[0]
     return str(pub_key_obj.encrypt(msg))
 
 
-def decrypt(encrypted_data: str, key: pgpy.PGPKey):
-    encrypted_msg = pgpy.PGPMessage.from_blob(encrypted_data)
+def decrypt(encrypted_data: str, key: PGPKey):
+    encrypted_msg = PGPMessage.from_blob(encrypted_data)
     msg = key.decrypt(encrypted_msg)
     return str(msg.message)
 
