e1b60a502dede3542dc05d1f47e377d686ab1440
Ehaab Basil <ehaabbasil1@gmail.com>
2020-07-29 17:28:16 +0800

    add keygen script.

---
+++ keygen.py
@@ -0,0 +1,23 @@
+from pgpy import PGPKey, PGPMessage, PGPUID
+from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
+from typer import run, secho, colors
+
+
+def generate(username: str):
+    primary_key = PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
+    primary_key.add_uid(
+            PGPUID.new(username),
+            usage={KeyFlags.EncryptCommunications},
+            uidhashes=[HashAlgorithm.SHA512],
+            ciphers=[SymmetricKeyAlgorithm.AES256],
+            compression=[CompressionAlgorithm.ZIP]
+    )
+
+    secho("Primary key, to be saved in server", fg=colors.BLUE)
+    secho(str(primary_key), fg=colors.YELLOW)
+    secho("Public key, to be saved in server", fg=colors.BLUE)
+    secho(str(primary_key.pubkey), fg=colors.GREEN)
+
+
+if __name__ == '__main__':
+    run(generate)
